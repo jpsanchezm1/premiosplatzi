2d396f6b4a1828a80e7736ca1a2e84477b8ffb19
jpsanchezm1 <jp.sanchezm1@uniandes.edu.co>
2022-12-20 23:13:58 -0500

    Curso Básico de Django

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,56 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
+from django.views import generic
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello World")
+
+'''def index(request):
+    latest_question_list= Question.objects.all()
+    return render(request,"polls/index.html", {'latest_question_list':latest_question_list})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/detail.html",{'question':question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/results.html",{'question':question})'''
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    template_name = "polls/detail.html"
+    model = Question
+
+class ResultView(generic.DetailView):
+    template_name = "polls/results.html"
+    model = Question
+
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.question_choices.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist): 
+        return render(request,"polls/detail.html",{"question":question,
+            "error_message":"No elegiste una respuesta"
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results",args=(question.id,)))
+
+def thanks(request):
+    return render(request,"polls/thanks.html")
+    
+
+    #render(request,"polls/thanks.html")
